5424b2f4b458c9b3b100d44365238b3c47c14b63
Heriberto Fabian <fabiansantiago87@gmail.com>
2025-03-07 17:14:27 -0600

    aplicando mejoras a los metodos en la clase Main

--- mi-proyecto/src/main/java/com/miempresa/App.java
+++ mi-proyecto/src/main/java/com/miempresa/App.java
@@ -13,11 +13,11 @@ public class App
 		System.out.println( "Hello World! desde rama 4" );
     }
 	
-	public String dgetMessage(){
+	public static String dgetMessage(){
 		Return "Hola desde feature 2";
 	}
 	
-	public String getMessage3(){
+	public static String getMessage3(){
 		Return "Este viene de la rama 3";
 	}
 }
